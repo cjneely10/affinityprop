e0e64e036c00891d263defb3c8e29a5fa3f6f894
Christopher Neely <christopher.neely1200@gmail.com>
2022-01-16 14:52:39 -0800

    Add additional test data; Implement auto-median preference

--- tests/external_datasets.rs
+++ tests/external_datasets.rs
@@ -6,10 +6,10 @@ use std::io::{BufRead, BufReader};
 use std::path::{Path, PathBuf};
 use std::str::FromStr;
 
-use ndarray::{Array2, Axis};
+use ndarray::{Array2, ArrayView1, Axis};
 use num_traits::Float;
 
-use affinityprop::{AffinityPropagation, NegEuclidean};
+use affinityprop::{AffinityPropagation, NegCosine, NegEuclidean, Similarity};
 
 fn load_data<F>(test_file: PathBuf) -> std::io::Result<(Array2<F>, HashMap<usize, Vec<usize>>)>
 where
@@ -44,12 +44,30 @@ where
     Ok((out, test_data_map))
 }
 
-fn run_test(path: PathBuf) {
-    let (test_array, test_results) = load_data::<f32>(path).unwrap();
-    let ap = AffinityPropagation::new(-1000., 0.5, 4, 400, 4000);
-    let (converged, results) = ap.predict(&test_array, NegEuclidean::default());
+#[derive(Default, Clone)]
+struct AbsLogEuclidean;
+
+impl<F> Similarity<F> for AbsLogEuclidean
+where
+    F: Float + Send + Sync,
+{
+    fn similarity(&self, a: &ArrayView1<F>, b: &ArrayView1<F>) -> F {
+        let mut row_diff = a - b;
+        row_diff.map_inplace(|_a| *_a = (*_a).powi(2).log2());
+        row_diff.sum().abs()
+    }
+}
+
+fn run_test<F, S>(ap: &AffinityPropagation<F>, s: S, path: PathBuf)
+where
+    F: Float + Send + Sync + FromStr + Default,
+    S: Similarity<F>,
+    <F as FromStr>::Err: Debug,
+{
+    let (test_array, actual) = load_data::<F>(path).unwrap();
+    let (converged, test_results) = ap.predict(&test_array, s);
     assert!(converged);
-    assert_eq!(test_results.len(), results.len());
+    assert_eq!(actual.len(), test_results.len());
 }
 
 fn file<A: AsRef<OsStr>>(path: A) -> PathBuf {
@@ -59,10 +77,36 @@ fn file<A: AsRef<OsStr>>(path: A) -> PathBuf {
 
 #[test]
 fn ten_exemplars() {
-    run_test(file(&"near-exemplar-10.test"));
+    let ap = AffinityPropagation::<f32>::new(Some(-1000.), 0.5, 4, 400, 4000);
+    run_test(&ap, NegEuclidean::default(), file(&"near-exemplar-10.test"));
 }
 
 #[test]
 fn fifty_exemplars() {
-    run_test(file(&"near-exemplar-50.test"));
+    let ap = AffinityPropagation::<f32>::new(Some(-1000.), 0.5, 4, 400, 4000);
+    run_test(&ap, NegEuclidean::default(), file(&"near-exemplar-50.test"));
 }
+
+// #[test]
+// fn iris() {
+//     let ap = AffinityPropagation::<f32>::new(None, 0.95, 4, 400, 4000);
+//     run_test(&ap, NegEuclidean::default(), file(&"iris.test"));
+// }
+
+#[test]
+fn breast_cancer() {
+    let ap = AffinityPropagation::<f32>::new(Some(-10000.), 0.95, 4, 400, 4000);
+    run_test(&ap, AbsLogEuclidean::default(), file(&"breast_cancer.test"))
+}
+
+// #[test]
+// fn diabetes() {
+//     let ap = AffinityPropagation::<f32>::new(None, 0.5, 4, 400, 4000);
+//     run_test(&ap, NegCosine::default(), file(&"diabetes.test"))
+// }
+
+// #[test]
+// fn digits() {
+//     let ap = AffinityPropagation::<f32>::new(-1000., 0.95, 4, 400, 4000);
+//     run_test(&ap, NegEuclidean::default(), file(&"digits.test"))
+// }
--- tests/generate_data.py
+++ tests/generate_data.py
@@ -11,21 +11,46 @@ Usage:
 import os
 import shutil
 
-from sklearn.datasets import make_blobs
+import sklearn.datasets as datasets
 
 DATA_DIR = "test-data"
 
 
-def generate(num_clusters: int, num_samples: int, dim: int, cluster_std: float):
+def prepared_x_y(fxn):
+    def decorated(*args, **kwargs):
+        return fxn(return_X_y=True, *args, **kwargs)
+    return decorated
+
+
+test_datasets = {
+    "iris": prepared_x_y(datasets.load_iris),
+    "diabetes": prepared_x_y(datasets.load_diabetes),
+    "digits": prepared_x_y(datasets.load_digits),
+    "breast_cancer": prepared_x_y(datasets.load_breast_cancer)
+}
+
+
+def write(test_file, label, point):
+    # label [data...]
+    test_file.write(str(int(label)))
+    for p in point:
+        test_file.write(" ")
+        test_file.write(str(p))
+    test_file.write("\n")
+
+
+def generate_gaussian(num_clusters: int, num_samples: int, dim: int, cluster_std: float):
     with open(f"{DATA_DIR}/near-exemplar-{num_clusters}.test", "w") as test_file:
-        for point, label in zip(*make_blobs(n_samples=num_samples, centers=num_clusters, n_features=dim, random_state=0,
-                                            cluster_std=cluster_std)):
-            # label [data...]
-            test_file.write(str(label))
-            for p in point:
-                test_file.write(" ")
-                test_file.write(str(p))
-            test_file.write("\n")
+        for point, label in zip(*datasets.make_blobs(n_samples=num_samples, centers=num_clusters, n_features=dim,
+                                                     random_state=0, cluster_std=cluster_std)):
+            write(test_file, label, point)
+
+
+def generate_prepared():
+    for key in test_datasets.keys():
+        with open(f"{DATA_DIR}/{key}.test", "w") as test_file:
+            for point, label in zip(*test_datasets[key]()):
+                write(test_file, label, point)
 
 
 if __name__ == "__main__":
@@ -34,6 +59,6 @@ if __name__ == "__main__":
     os.makedirs(DATA_DIR)
 
     # 10 clusters, 100 data points
-    generate(10, 300, 50, 1.0)
-    generate(50, 300, 50, 1.0)
-
+    generate_gaussian(10, 300, 50, 1.0)
+    generate_gaussian(50, 300, 50, 1.0)
+    generate_prepared()
