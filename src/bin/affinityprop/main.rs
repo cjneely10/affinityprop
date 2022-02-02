01b156258178644b216d285795419af95bc8c1ee
Christopher Neely <christopher.neely1200@gmail.com>
2022-02-02 09:32:50 -0800

    Add flag to set if ids are present in file

--- src/bin/affinityprop/main.rs
+++ src/bin/affinityprop/main.rs
@@ -22,6 +22,7 @@ fn main() {
         (about: "Vectorized and Parallelized Affinity Propagation")
         (@arg INPUT: -i --input +takes_value +required "Path to input file")
         (@arg DELIMITER: -l --delimiter +takes_value "File delimiter, default '\\t'")
+        (@arg NO_LABELS: -n --no_labels "Input file does not contain IDS as the first column")
         (@arg PREF: -p --preference +takes_value +allow_hyphen_values "Preference to be own exemplar, default=median pairwise similarity")
         (@arg MAX_ITER: -m --max_iter +takes_value "Maximum iterations, default=100")
         (@arg CONV_ITER: -c --convergence_iter +takes_value "Convergence iterations, default=10")
@@ -108,10 +109,12 @@ fn main() {
     }
     // Run AP
     let is_precalculated = similarity == 3;
+    let collect_labels = !matches.is_present("NO_LABELS");
     match precision {
         "f64" => match from_file::<f64>(
             Path::new(&input_file).to_path_buf(),
             delimiter,
+            collect_labels,
             is_precalculated,
         ) {
             Ok((x, y)) => {
@@ -132,6 +135,7 @@ fn main() {
         _ => match from_file::<f32>(
             Path::new(&input_file).to_path_buf(),
             delimiter,
+            collect_labels,
             is_precalculated,
         ) {
             Ok((x, y)) => {
